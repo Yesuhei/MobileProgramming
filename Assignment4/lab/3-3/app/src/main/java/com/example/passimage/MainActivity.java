9cacb48fcaacb2b85797499a5d0e56042b0ba3e1
Yesuhei <yesuhei0524@gmail.com>
2021-04-15 20:32:11 +0800

    Added Assignment 5

--- Assignment4/lab/3-3/app/src/main/java/com/example/passimage/MainActivity.java
+++ Assignment4/lab/3-3/app/src/main/java/com/example/passimage/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn;
+    private Spinner sp;
+    private String[] user = {"User 1","User 2","User 3","User 4"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = (Button) findViewById(R.id.btn);
+        sp = (Spinner) findViewById(R.id.sp);
+        ArrayAdapter adapter = new ArrayAdapter(
+                this, android.R.layout.simple_spinner_item, user
+        );
+        sp.setAdapter(adapter);
         btn.setOnClickListener(v -> {
-            Intent i = new Intent(MainActivity.this,)
+
+            int pos = sp.getSelectedItemPosition();
+
+            Intent i = new Intent(MainActivity.this,MainActivity2.class);
+            i.putExtra("pic",pos);
+            startActivity(i);
 
         });
     }
--- Assignment4/lab/3-3/app/src/main/java/com/example/passimage/MainActivity2.java
+++ Assignment4/lab/3-3/app/src/main/java/com/example/passimage/MainActivity2.java
@@ -2,13 +2,24 @@ package com.example.passimage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private TextView tv;
+    private ImageView iv;
+    private int[] img = {R.drawable.user1,R.drawable.user2,R.drawable.user3,R.drawable.user4};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Bundle bundle = getIntent().getExtras();
+        int pos = bundle.getInt("pic");
+        iv.setImageResource(img[pos]);
     }
 }
